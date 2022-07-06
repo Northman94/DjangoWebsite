2f6bb19855e5261bdb553114c60e3feb064be571
Northman94 <Zhandarm2000@gmail.com>
2022-07-06 12:01:05 +0300

    adit view with .html file

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('book/<int:book_id>', views.book_by_id, name = 'book_by_id')
 ]
 
 
